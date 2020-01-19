cb54f202a1556d93c901f6b548222076b0b4bb9f
Adam White <adamfrankwhite@gmail.com>
2020-01-19 12:42:10 +0000

    comment bodyparser

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 app.use(express.json())
 
 const dbURI = process.env.ATLAS_URI
