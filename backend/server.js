f2c0f25dd57337b7c25777d371648f1a955e293d
Adam White <adamfrankwhite@gmail.com>
2020-01-19 12:43:45 +0000

    delete parser

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+
 app.use(express.json())
 
 const dbURI = process.env.ATLAS_URI
