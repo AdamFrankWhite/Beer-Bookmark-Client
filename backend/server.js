4bd63de374e37030b4739a0b2844598714c9473b
Adam White <adamfrankwhite@gmail.com>
2020-01-19 11:47:11 +0000

    added http

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const bodyParser = require('body-parser')
-
+const http = require('http')
 require('dotenv').config()
 const app = express()
-// const port = process.env.PORT || 5000
+
+const server = http.Server(app)
+const port = process.env.PORT || 5000
 
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(express.json())
 
 const dbURI = process.env.ATLAS_URI
@@ -27,7 +26,7 @@ app.use('/users', usersRouter)
 
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up and running: port ${port}`)
 })
 
