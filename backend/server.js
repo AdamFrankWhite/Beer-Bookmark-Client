7ae63891f4774730b0accf3c69bfdd022b06bb91
Adam White <adamfrankwhite@gmail.com>
2020-01-19 10:04:37 +0000

    add bodyparser

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 require('dotenv').config()
 const app = express()
 // const port = process.env.PORT || 5000
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json())
 
 const dbURI = process.env.ATLAS_URI
