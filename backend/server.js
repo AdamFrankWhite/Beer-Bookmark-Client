2611eff3848d4b7e6e0aa6b8da87ba71c81eff45
Adam White <adamfrankwhite@gmail.com>
2020-01-19 15:09:29 +0000

    proxy

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT || 80
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
