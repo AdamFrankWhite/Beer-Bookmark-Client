a8eb2ec611aeeccfbcc8214688b0bd7e1c73365f
Adam White <adamfrankwhite@gmail.com>
2020-01-23 17:31:42 +0000

    heroku changed paths/proc

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT
 
 app.use(cors())
 
