796ee979b34129bcff01d6fec06dfd860e4362db
Adam White <adamfrankwhite@gmail.com>
2020-02-09 11:40:00 +0000

    added sort function to mybeers

--- src/comp/MyBeers.js
+++ src/comp/MyBeers.js
@@ -10,26 +10,42 @@ class MyBeers extends React.Component {
             favouriteBeers: [],
             isLoading: true
         }
-    //     this.addBeer = this.addBeer.bind(this)
+        this.sortBeers = this.sortBeers.bind(this)
     }
 
-    // addBeer(id) {
-    //     console.log(`Beer ${id} added`)
-    // }
+   
     componentDidMount() {
-        this.setState({isLoading: false})
+        this.setState({isLoading: false, beerData: this.props.favouriteBeers})
     }
-    componentDidUpdate() {
-        console.log(this.props.favouriteBeers)
+   
+    sortBeers(searchType) {
+        let sortedBeers = this.state.beerData.sort((a, b) => (a[searchType] > b[searchType]) ? 1 : ((b[searchType] > a[searchType]) ? -1 : 0))
+        this.setState({beerData: sortedBeers})
+        console.log(sortedBeers)
     }
 
-    //TODO - need MyBeers state to update when App state updates - which it isn't currently.  Consider removing MyBeers state, take App state as props
     render() {
         const center = {margin: "auto"}
-        const beers = this.props.favouriteBeers.map(beer => <Beer myBeers={true} favouriteBeers={this.props.favouriteBeers} beerData={beer} src={beer.img} id={beer._id} name={beer.name} abv={beer.abv} description={beer.description} addBeer={this.props.addBeer} deleteBeer={this.props.deleteBeer} updateBeer={this.props.updateBeer}/>)
+        const beers = this.state.beerData.map(beer => 
+            <Beer 
+                myBeers={true} 
+                favouriteBeers={this.props.favouriteBeers} 
+                beerData={beer} 
+                src={beer.img} 
+                id={beer._id} 
+                name={beer.name} 
+                abv={beer.abv} 
+                description={beer.description} 
+                addBeer={this.props.addBeer} 
+                deleteBeer={this.props.deleteBeer} 
+                updateBeer={this.props.updateBeer}
+            />)
         return (
             <div className="App">
                 <h1 className="heading">My Favourite Beers</h1>
+                <span>Sort by </span>
+                <span className="sort-btn" onClick={() => this.sortBeers("beerName")}>Name</span>
+                <span className="sort-btn" onClick={() => this.sortBeers("stars")}>Top Rated</span>
                 {this.state.isLoading && <ReactLoading style={center} type="bubbles" color="black" height={400} width={350} />}
                 <div className="beer-container">  
                     {beers}
