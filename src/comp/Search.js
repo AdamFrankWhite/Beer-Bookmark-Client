88c6c300c54cfd3a6cf3a8e8f78fa0263119ea96
Adam White <adamfrankwhite@gmail.com>
2020-10-24 12:39:01 +0100

    add contact object props

--- src/comp/Search.js
+++ src/comp/Search.js
@@ -1,82 +1,103 @@
-import React from 'react'
-import Beer from './Beer'
-import Brewery from './Brewery'
-import ReactLoading from 'react-loading'
+import React from "react";
+import Beer from "./Beer";
+import Brewery from "./Brewery";
+import ReactLoading from "react-loading";
 
 class Search extends React.Component {
     constructor() {
-        super()
+        super();
         this.state = {
             beerData: [],
             favouriteBeers: [],
-            isLoading: false
-        }
+            isLoading: false,
+        };
     }
 
     render() {
-        const center = {margin: "auto", height: 150, width: 200}
+        const center = { margin: "auto", height: 150, width: 200 };
 
         // Beer Components
-        const beers = this.props.beerData.map(item => 
-            <Beer 
-                search={true} 
-                src={item.beer.beer_label} 
-                id={item.beer.bid} 
-                name={item.beer.beer_name} 
-                abv={item.beer.beer_abv} 
-                style={item.beer.beer_style}
-                description={item.beer.beer_description} 
-                beerData={item.beer} 
-                favouriteBeers={this.props.favouriteBeers} 
-                addBeer={this.props.addBeer} 
-                loggedIn={this.props.loggedIn}
-                brewery={item.brewery.brewery_name} 
-            />)
+        const beers = this.props.beerData.map((item) => {
+            return (
+                <Beer
+                    search={true}
+                    src={item.beer.beer_label}
+                    id={item.beer.bid}
+                    name={item.beer.beer_name}
+                    abv={item.beer.beer_abv}
+                    style={item.beer.beer_style}
+                    description={item.beer.beer_description}
+                    beerData={item.beer}
+                    favouriteBeers={this.props.favouriteBeers}
+                    addBeer={this.props.addBeer}
+                    loggedIn={this.props.loggedIn}
+                    brewery={item.brewery}
+                />
+            );
+        });
 
         // Brewery Components
-        const breweries = this.props.breweryData.map(item => 
-            <Brewery 
+        const breweries = this.props.breweryData.map((item) => (
+            <Brewery
                 img={item.brewery.brewery_label}
                 name={item.brewery.brewery_name}
                 location={item.brewery.location.brewery_city}
                 beerNum={item.brewery.beer_count}
             />
-            )
+        ));
         return (
             <div className="App">
-
                 {/* Loading animation */}
-                {this.state.isLoading && <ReactLoading style={center} type="bubbles" color="black" />}
-                
-                {/* Loaded  */}
-                {!this.state.isLoading &&
-                <div> 
-                    <p>Can't quite remember that awesome beer you had last night? Bookmark it with BeerMe</p>
+                {this.state.isLoading && (
+                    <ReactLoading style={center} type="bubbles" color="black" />
+                )}
 
-                    {/* Search Box */}
-                    <label htmlFor="searchTerm">Search: 
-                    <input type="text" value={this.props.searchTerm} onChange={this.props.handleChange} name="searchTerm"></input>
-                    </label>
-                    <button onClick={() => this.props.searchBeer(this.props.searchTerm, this.props.searchType)} >Go</button>
-                    <br></br>
-                    {/* <label htmlFor="searchType">Beer: 
+                {/* Loaded  */}
+                {!this.state.isLoading && (
+                    <div>
+                        <p>
+                            Can't quite remember that awesome beer you had last
+                            night? Bookmark it with BeerMe
+                        </p>
+
+                        {/* Search Box */}
+                        <label htmlFor="searchTerm">
+                            Search:
+                            <input
+                                type="text"
+                                value={this.props.searchTerm}
+                                onChange={this.props.handleChange}
+                                name="searchTerm"
+                            ></input>
+                        </label>
+                        <button
+                            onClick={() =>
+                                this.props.searchBeer(
+                                    this.props.searchTerm,
+                                    this.props.searchType
+                                )
+                            }
+                        >
+                            Go
+                        </button>
+                        <br></br>
+                        {/* <label htmlFor="searchType">Beer: 
                         <input name="searchType" type="radio" value="beer" onChange={this.props.handleChange}></input>
                     </label>
                     <label htmlFor="searchType">Brewery: 
                         <input name="searchType" type="radio" value="brewery" onChange={this.props.handleChange}></input>
                     </label> */}
-                    
-                    {/* Beer Container */}
-                    <div className="beer-container">
-                        {beers}
-                        {breweries}
+
+                        {/* Beer Container */}
+                        <div className="beer-container">
+                            {beers}
+                            {breweries}
+                        </div>
                     </div>
-                </div>}
-                
+                )}
             </div>
-        )
+        );
     }
-    
 }
 
-export default Search
+export default Search;
