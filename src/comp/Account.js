48099b06995d1c853cf7d9537b5772914b35bd8f
Adam White <adamfrankwhite@gmail.com>
2020-10-28 23:20:57 +0000

    slide toggle

--- src/comp/Account.js
+++ src/comp/Account.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { setColorScheme } from "../redux/actions/userActions";
 function Account(props) {
+    const [checked, setChecked] = useState(false);
+    useEffect(() => {
+        !checked ? props.setColorScheme("light") : props.setColorScheme("dark");
+    }, [checked]);
     return (
         <div>
             <h1>Account</h1>
             <h2>Set Color Scheme</h2>
-            <h3 onClick={() => props.setColorScheme("light")}>Light</h3>
-            <h3 onClick={() => props.setColorScheme("dark")}>Dark</h3>
+            <div className="slider-div">
+                <p>Light</p>
+                <label class="switch">
+                    <input
+                        type="checkbox"
+                        onChange={(e) => setChecked(e.target.checked)}
+                    />
+                    <span class="slider round"></span>
+                </label>
+                <p>Dark</p>
+            </div>
         </div>
     );
 }
