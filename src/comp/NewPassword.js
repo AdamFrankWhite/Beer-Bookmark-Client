203cc9c7c81b6533247a27c008a5ac01ef1c5c9e
Adam White <adamfrankwhite@gmail.com>
2020-10-29 22:43:06 +0000

    change password working

--- src/comp/NewPassword.js
+++ src/comp/NewPassword.js
@@ -1,15 +1,35 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 const NewPassword = ({ match, location }) => {
     const { search } = location;
     const queryRegEx = /\?q=/;
     //Remove query string
     const requestId = search.replace(queryRegEx, "");
+
+    //New password
+    const [newPassword, setNewPassword] = useState("");
     return (
         <div>
             <h1>New Password</h1>
-            <input type="password" />
-            <button onClick={() => console.log(requestId)}>Submit</button>
+            <input
+                type="password"
+                onChange={(e) => setNewPassword(e.target.value)}
+            />
+            <button
+                onClick={() => {
+                    axios
+                        .put("http://localhost:5000/users/reset", {
+                            id: requestId,
+                            newPassword,
+                        })
+                        .then((res) => {
+                            console.log(res.data);
+                        });
+                    console.log(requestId, newPassword);
+                }}
+            >
+                Submit
+            </button>
         </div>
     );
 };
