86ce2338748f7afa780b74ec8801f24bd8763929
Adam White <adamfrankwhite@gmail.com>
2020-11-02 12:29:20 +0000

    created footer

---
+++ src/comp/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="footer-cont">
+                <div className="about">
+                    <div className="logo-cont">
+                        <img src="/logo.png" alt="BeerBookmark logo" />
+                        <div className="text-cont">
+                            <h1>Beer</h1>
+                            <h2>Bookmark</h2>
+                        </div>
+                    </div>
+                    <p>
+                        BeerBookmark is a bookmarking app for beer and cider
+                        lovers. Keep your favourite beers, ratings and comments
+                        in a single place.
+                    </p>
+                </div>
+                <div className="links">
+                    <h3>Links</h3>
+                    <ul>
+                        <li>Contact</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+                <div className="subscribe">
+                    <h3>Subscribe</h3>
+                    <ul>
+                        <li>Sign Up</li>
+                        <li>App</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+}
