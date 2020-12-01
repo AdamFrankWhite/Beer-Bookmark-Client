85ffeb0dfcee846ec9eef38667a7a1c150f919a8
Adam White <adamfrankwhite@gmail.com>
2020-12-01 14:18:30 +0000

    snazzy nav icons

--- src/comp/NavBar.js
+++ src/comp/NavBar.js
@@ -2,6 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { logout } from "../redux/actions/userActions";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBeer } from "@fortawesome/free-solid-svg-icons/faBeer";
+import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons/faUserCircle";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons/faSignOutAlt";
+import { faSignInAlt } from "@fortawesome/free-solid-svg-icons/faSignInAlt";
+import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
 function NavBar(props) {
     return (
         <header>
@@ -21,34 +29,57 @@ function NavBar(props) {
                         </div>
                     </div>
                 </Link>
-                <div>
+                <div className="flex-row">
                     {props.user.loggedIn ? (
                         <Link to="/my-beers" className="link">
-                            My Beers
+                            <div className="flex-col">
+                                <FontAwesomeIcon icon={faBeer} />
+                                <span className="nav-icon-text">My Beers</span>
+                            </div>
                         </Link>
                     ) : null}
 
                     <Link to="/search" className="link">
-                        Search
+                        <div className="flex-col">
+                            <FontAwesomeIcon icon={faSearch} />
+                            <span className="nav-icon-text">Search</span>
+                        </div>
                     </Link>
                     {/* <Link to="/random-beer" className="link">
                         Random Beer
                     </Link> */}
                     {!props.user.loggedIn && (
                         <Link to="/register" className="link">
-                            Register
+                            <div className="flex-col">
+                                <FontAwesomeIcon icon={faUser} />
+                                <span className="nav-icon-text">Register</span>
+                            </div>
                         </Link>
                     )}
                     {props.user.loggedIn ? (
                         <Link to="/settings" className="link">
-                            Settings
+                            <div className="flex-col">
+                                <FontAwesomeIcon icon={faUserCircle} />
+                                <span className="nav-icon-text">Account</span>
+                            </div>
                         </Link>
                     ) : null}
                     <Link to="/login" className="link">
                         {props.user.loggedIn ? (
-                            <span onClick={() => props.logout()}>Logout</span>
+                            <div className="flex-col">
+                                <FontAwesomeIcon icon={faSignOutAlt} />
+                                <span
+                                    className="nav-icon-text"
+                                    onClick={() => props.logout()}
+                                >
+                                    Logout
+                                </span>
+                            </div>
                         ) : (
-                            <span>Login</span>
+                            <div className="flex-col">
+                                <FontAwesomeIcon icon={faSignInAlt} />
+                                <span className="nav-icon-text">Login</span>
+                            </div>
                         )}
                     </Link>
                 </div>
